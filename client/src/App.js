3992a9fff10771dcf3619354727bf5cf9a41772c
GuillermoCzza <guiczza@gmail.com>
2023-12-24 16:48:19 -0300

    small additions to client

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,19 @@ import language from './language.json';
 
 
 function Tabla(tabla) {
+
+
 	const columnOrder = []; //necessary to write values of rows in same order
 	const filas = []; //this will contain the JSX of all the rows
 
 	//create table headers and record column order for the fields
 	const headersJSX = []
 	tabla.fields.forEach(field => {
-		headersJSX.push(<div className='table-cell'><p>{field.name.toUpperCase()}</p></div>)
+		const fieldName = field.name.toUpperCase();
+		headersJSX.push(<div key={`${tabla.tableName}-${field.name}-header`} className='table-cell'><p>{fieldName}</p></div>)
 		columnOrder.push(field.name);
 	});
-	filas.push(<div className='table-row table-headers'>{headersJSX}</div>);
+	filas.push(<div key={`${tabla.tableName}-headers-row`} className='table-row table-headers'>{headersJSX}</div>);
 
 	//create each row
 	tabla.rows.forEach(row => {
@@ -30,13 +33,15 @@ function Tabla(tabla) {
 
 		//actually write the JSX for the row with the ordered values
 		rowCellValues.forEach((value, index) => {
-			rowJSX.push(<div className='table-cell'><input name={rowCellValues[index]} value={value} type='text'></input></div>);
+			rowJSX.push(<div className='table-cell'>
+				<input name={rowCellValues[index]} defaultValue={value} type='text' />
+			</div>);
 		});
 
 		//add the JSX of each row to 'filas'
-		filas.push(<form action={`${config.SERVER_ADDRESS}/tables/${tabla.tableName}`} className='table-row' method="PUT">
-				{rowJSX}
-			</form>
+		filas.push(<form key={`${tabla.tableName}-${row['producto_id']}`} action={`${config.SERVER_ADDRESS}/tables/${tabla.tableName}`} className='table-row' method="PUT">
+			{rowJSX}
+		</form>
 		);
 	});
 
